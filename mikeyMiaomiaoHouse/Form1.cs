58e29e5b695ed40f2bc5e423d3f11c26aa32c7b3
Creeper233 <52315359+Creeper2333@users.noreply.github.com>
2020-06-07 08:46:40 +0800

    Add files via upload

---
+++ mikeyMiaomiaoHouse/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using System.IO;
+
+namespace mikeyMiaomiaoHouse
+{
+    public partial class Form1 : Form
+    {
+
+
+        #region u盘属性
+        public const int WM_DEVICECHANGE = 0x219;//U盘插入后，OS的底层会自动检测到，然后向应用程序发送“硬件设备状态改变“的消息
+        public const int DBT_DEVICEARRIVAL = 0x8000;  //就是用来表示U盘可用的。一个设备或媒体已被插入一块，现在可用。
+        public const int DBT_CONFIGCHANGECANCELED = 0x0019;  //要求更改当前的配置（或取消停靠码头）已被取消。
+        public const int DBT_CONFIGCHANGED = 0x0018;  //当前的配置发生了变化，由于码头或取消固定。
+        public const int DBT_CUSTOMEVENT = 0x8006; //自定义的事件发生。 的Windows NT 4.0和Windows 95：此值不支持。
+        public const int DBT_DEVICEQUERYREMOVE = 0x8001;  //审批要求删除一个设备或媒体作品。任何应用程序也不能否认这一要求，并取消删除。
+        public const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;  //请求删除一个设备或媒体片已被取消。
+        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;  //一个设备或媒体片已被删除。
+        public const int DBT_DEVICEREMOVEPENDING = 0x8003;  //一个设备或媒体一块即将被删除。不能否认的。
+        public const int DBT_DEVICETYPESPECIFIC = 0x8005;  //一个设备特定事件发生。
+        public const int DBT_DEVNODES_CHANGED = 0x0007;  //一种设备已被添加到或从系统中删除。
+        public const int DBT_QUERYCHANGECONFIG = 0x0017;  //许可是要求改变目前的配置（码头或取消固定）。
+        public const int DBT_USERDEFINED = 0xFFFF;  //此消息的含义是用户定义的
+        #endregion
+
+
+
+
+
+        #region 检测u盘
+        protected override void WndProc(ref Message m)
+        {
+            try
+            {
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            DriveInfo[] drives = DriveInfo.GetDrives();
+                            foreach (DriveInfo d in drives)
+                            {
+                                if (d.DriveType == DriveType.Removable)
+                                {
+                                    Console.WriteLine(d.Name);
+                                    Ulog.EjectUsb eju = new Ulog.EjectUsb(d.Name);
+                                    bool result = eju.Eject();
+                                    if (result == true)
+                                    {
+                                        MessageBox.Show("振兴妙妙屋开启！！！");
+                                    }
+                                }
+
+                            }
+                            break;
+                        case DBT_DEVICEARRIVAL://U盘插入
+                            
+                            break;
+                        case DBT_CONFIGCHANGECANCELED:
+                            break;
+                        case DBT_CONFIGCHANGED:
+                            break;
+                        case DBT_CUSTOMEVENT:
+                            break;
+                        case DBT_DEVICEQUERYREMOVE:
+                            break;
+                        case DBT_DEVICEQUERYREMOVEFAILED:
+                            break;
+                        case DBT_DEVICEREMOVECOMPLETE: //U盘卸载
+                            
+                            break;
+                        case DBT_DEVICEREMOVEPENDING:
+                            break;
+                        case DBT_DEVICETYPESPECIFIC:
+                            break;
+                        case DBT_DEVNODES_CHANGED:
+                            break;
+                        case DBT_QUERYCHANGECONFIG:
+                            break;
+                        case DBT_USERDEFINED:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            base.WndProc(ref m);
+        }
+        #endregion
+
+
+        System.Timers.Timer t = new System.Timers.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in drives)
+            {
+                if (d.DriveType == DriveType.Removable)
+                {
+                    Console.WriteLine(d.Name);
+                    Ulog.EjectUsb eju = new Ulog.EjectUsb(d.Name);
+                    bool result = eju.Eject();
+                    if (result == true)
+                    {
+                        MessageBox.Show("振兴妙妙屋开启！！！");
+                    }
+                }
+
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Hide();
+            t.Interval = 1000;
+            t.Elapsed += new ElapsedEventHandler(t_Elapsed);
+            t.Start();
+        }
+
+    }
+}
